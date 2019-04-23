a145651db62f62f8673c8cd068d58a84fb277b71
Chris Pak <chris.pak125@gmail.com>
2019-04-23 17:27:59 -0400

    Set up populateVoices function and voicesChanged event listener

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const msg = new SpeechSynthesisUtterance()
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]')
-const options = document.querySelectorAll('[type="range"], [name="tet"]')
+const options = document.querySelectorAll('[type="range"], [name="text"]')
 const speakButton = document.querySelector('#speak')
 const stopButton = document.querySelector('#stop')
+msg.text = document.querySelector('[name="text"]').value
+
+
+function populateVoices() {
+    voices = this.getVoices()
+    voicesDropdown.innerHTML = voices.map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang}) </option>`).join('')
+}
+
+function setVoice(){
+    console.log("Changing voice")
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices)
+voicesDropdown.addEventListener("change", setVoice)
